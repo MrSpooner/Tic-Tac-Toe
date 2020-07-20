f58ca05ba7f9149399f256912c8bae548756d670
MrSpooner <spoontamer7@gmail.com>
2020-07-20 09:03:44 +0500

    Fixed

--- app/plugins/script.js
+++ app/plugins/script.js
@@ -15,7 +15,7 @@ const comboWin = [
 ]
 let field = ['', '', '', '', '', '', '', '', ''];
 let turn = true;
-//Добавляется скрытая кнопку рестарта
+//Добавляется скрытая кнопка рестарта
 let button = document.querySelector('.navbar__button_play');
 button.addEventListener('click', restartGame);
 button.style.display = "none";
@@ -27,44 +27,47 @@ function start() {
 }
 //Задается поведение компьютера во время игры
 function pcStep() {
-    fieldCell.forEach(cell => {
-        cell.removeEventListener('click', clickCell)
-    })
+
     const sources = Array.from(document.getElementsByClassName('cell'));
     let index;
 
-    while (!checkCell(index)) {
+    do {
         if (!isDraw())
             index = Math.floor(Math.random() * 9);
         else
             break;
-    }
-    mark(sources[index], cellO);
+    } while (!checkCell(index));
+
     if (!checkWinner(cellO) && (!checkWinner(cellX)) && (!isDraw())) {
+        mark(sources[index], cellO);
         swap();
         playerStep();
     }
     if (checkWinner(cellO) || isDraw()) {
         oScore += 1;
         console.log(isDraw());
-        if (isDraw()) {
+        if (isDraw() && !(checkWinner(cellO))) {
             xScore += 1;
         }
         document.getElementById("score").textContent = `Счет: ${xScore} : ${oScore}`;
         document.getElementById("results").textContent += `
         Счет: ${xScore} : ${oScore}`;
         button.style.display = "inherit";
+        fieldCell.forEach(cell => {
+            cell.removeEventListener('click', clickCell)
+        })
         restartButton();
 
     }
 
 }
-//Обработка действий игрока
+
 function playerStep() {
     fieldCell.forEach(cell => {
         cell.addEventListener('click', clickCell)
     })
 }
+//Проверка ячейки на возможность ее использования
 function checkCell(number) {
     if (field[number] !== '') {
         return false;
@@ -72,25 +75,30 @@ function checkCell(number) {
         return true;
     }
 }
+//Обработка хода игрока
 function clickCell(e) {
     const cell = e.target;
-
     let index = cell.dataset.number;
 
     if ((checkCell(index))) {
         mark(cell, cellX);
-        swap();
-        pcStep();
-        if (checkWinner(cellX) || isDraw()) {
+
+        if (checkWinner(cellX) || isDraw() && (!checkWinner(cellO))) {
+            fieldCell.forEach(cell => {
+                cell.removeEventListener('click', clickCell)
+            })
             xScore += 1;
-            if (isDraw()) {
-                oCore += 1;
+            if (isDraw() && !(checkWinner(cellX))) {
+                oScore += 1;
             }
             document.getElementById("score").textContent = `Счет: ${xScore} : ${oScore}`;
-            document.getElementById("results").textContent += `
-            Счет: ${xScore} : ${oScore}`;
+            document.getElementById("results").textContent += ` Счет: ${xScore} : ${oScore}`;
             button.style.display = "inherit";
             restartButton();
+        }
+        else {
+            swap();
+            pcStep();
         }
 
     }
@@ -135,5 +143,9 @@ function isDraw() {
         return cell.classList.contains(cellX) || cell.classList.contains(cellO)
     })
 }
+
+// Алгоритм поиска наилучшего хода
+// function miniMax{
+// }
 
 start();
